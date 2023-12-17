6e9eb7717ff2792d65c20b73dda296ef6ea78790
srinivasanv29 <116896908+srinivasanv29@users.noreply.github.com>
2023-12-17 22:17:03 +0000

    first commit

---
+++ String_Handling.py
@@ -0,0 +1,8 @@
+arn = "arn:aws:iam::123456789:user/test123"
+
+print(arn.split("/")) # // To split and print using field separator "/"
+
+print(arn.split("/")[1]) # // to print 1st index from split output
+
+name = "srini" 
+print(name.upper())  #// To conver to upper 
